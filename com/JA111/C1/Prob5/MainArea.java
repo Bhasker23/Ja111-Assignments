fe463c56da9948c6408e991f00449dc0af8f2f2f
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-05-23 15:56:58 +0530

    Q5 pushed

---
+++ com/JA111/C1/Prob5/MainArea.java
@@ -0,0 +1,13 @@
+package com.JA111.C1.Prob5;
+
+public class MainArea {
+
+	public static void main(String[] args) {
+		
+		Shapes s1 = new Shapes();
+		
+		s1.circleArea(2);
+		s1.rectangleArea(10, 10);
+		s1.squareArea(5);
+	}
+}
--- com/JA111/C1/Prob5/Shapes.java
+++ com/JA111/C1/Prob5/Shapes.java
@@ -1,8 +1,21 @@
 package com.JA111.C1.Prob5;
 
+
+
 public class Shapes {
 
-	public void area() {
+	public void circleArea(int radious) {
+		System.out.println(radious*2);
 		
 	}
+	
+	public void rectangleArea(int length, int breadth) {
+		System.out.println(length*breadth);
+	}
+	
+	public void squareArea(int side) {
+		System.out.println(4*side);
+	}
+	
 }
+
